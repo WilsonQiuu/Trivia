8d486ae9849011a1985bedcddfbb5980c6de54a8
Wilson Qiu <your_email@example.com>
2025-05-02 01:24:42 -0400

    fixedsa

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 
 const port = parseInt(process.env.PORT || "3000", 10);
+
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
